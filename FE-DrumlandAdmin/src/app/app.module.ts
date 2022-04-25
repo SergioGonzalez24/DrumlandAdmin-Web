6556815ff727d71f7d66c06876a1c8d6f3b5ce60
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-04-25 14:26:31 -0500

    dashboard

--- FE-DrumlandAdmin/src/app/app.module.ts
+++ FE-DrumlandAdmin/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -13,11 +15,19 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     AppComponent,
     LoginComponent,
     NavbarComponent,
+    DashboardComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule //Importar para usar formularios
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      {path: 'dashboard', component: DashboardComponent}
+
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
